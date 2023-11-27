0141e08a6c45f21deb23c7823c68426780186905
Булат Хайруллин <kagariakko74@gmail.com>
2023-11-27 15:31:43 +0300

    add menu

--- app/src/main/java/com/example/lab7/MainActivity.kt
+++ app/src/main/java/com/example/lab7/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lab7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
